00a662295b665e9149683b201422dca089d6fe3c
StefanoM98 <smartino030@gmail.com>
2023-03-13 14:28:07 +0100

    Add: prited in index

--- ex-dadi/js/script.js
+++ ex-dadi/js/script.js
@@ -6,8 +6,17 @@ console.log(rndNumberComputer);
 const rndNumberUser = Math.floor (Math.random() * (6 - 1 + 1)) + 1;
 console.log(rndNumberUser)
 
+alert ("Benvenuto nella booleanautomachine, subito chiuso questo messaggio di avviso avrai la possibilità di vincere un nuovo pc.....ti starai chiedendo il funzionamento del gioco giusto? Perfetto la risposta è......PURA FORTUNA. Saranno estratti due numeri da 1 a 6, uno per te e uno per il computer, se il numero del computer sarà piu altro del tuo allora avrai perso. Nel caso il tuo numero sarà più alto di quello del pc allora avrai vinto. Facci sapere se hai vinto oppure no. Buona Fortuna gamer.")
+
+let message = ""
 if (rndNumberComputer >= rndNumberUser) {
-    console.log ("RIPROVA, SARAI PIU' FORTUNATO")
+    message = "RIPROVA, SARAI PIU' FORTUNATO";
 } else if (rndNumberComputer < rndNumberUser) {
-    console.log("COMPLIMENTI, hai vinto un nuovo pc");
+    message = "COMPLIMENTI, hai vinto un nuovo pc";
 }
+console.log (message)
+
+document.getElementById("pc-number").innerHTML = `Il numero del computer è ${rndNumberComputer}`
+document.getElementById("your-number").innerHTML = `Il tuo numero è ${rndNumberUser}`
+document.getElementById("result").innerHTML = message
+
