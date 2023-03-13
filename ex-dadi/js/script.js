7b18ceeff7ed98a87272a1de71161a49976fadc9
StefanoM98 <smartino030@gmail.com>
2023-03-13 17:42:07 +0100

    Add: fixed draw

--- ex-dadi/js/script.js
+++ ex-dadi/js/script.js
@@ -6,7 +6,7 @@ console.log(rndNumberComputer);
 const rndNumberUser = Math.floor (Math.random() * (6 - 1 + 1)) + 1;
 console.log(rndNumberUser)
 
-alert ("Benvenuto nella booleanautomachine, subito chiuso questo messaggio di avviso avrai la possibilità di vincere un nuovo pc.....ti starai chiedendo il funzionamento del gioco giusto? Perfetto la risposta è......PURA FORTUNA. Saranno estratti due numeri da 1 a 6, uno per te e uno per il computer, se il numero del computer sarà piu altro del tuo allora avrai perso. Nel caso il tuo numero sarà più alto di quello del pc allora avrai vinto. Facci sapere se hai vinto oppure no. Buona Fortuna gamer.")
+alert ("Benvenuto nella booleanautomachine, subito chiuso questo messaggio di avviso avrai la possibilità di vincere un nuovo pc.....ti starai chiedendo il funzionamento del gioco giusto? Perfetto la risposta è......PURA FORTUNA. Saranno estratti due numeri da 1 a 6, uno per te e uno per il computer, se il numero del computer sarà piu altro del tuo allora avrai perso. Nel caso il tuo numero sarà più alto di quello del pc allora avrai vinto. Se invece dovessero uscire due numeri uguali ricorda che il banco, in questo caso in computer, vince sempre. Facci sapere se hai vinto oppure no. Buona Fortuna gamer.")
 
 let message = ""
 if (rndNumberComputer >= rndNumberUser) {
